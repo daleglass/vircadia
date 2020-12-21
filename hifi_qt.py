4c7b59e8d47a68da087d2c602a3bc4416cd7fc62
Dale Glass <dale@daleglass.net>
2020-12-21 20:58:11 +0100

    Improve Qt handling in CMake
    Support 3 options:
    
    	* VIRCADIA_USE_SYSTEM_QT is set -- use system's Qt
    	* VIRCADIA_QT_PATH is set -- use Qt found in that dir
    	* Otherwise -- use downloaded Qt package
    
    This removes these variables:
    	* VIRCADIA_USE_QT_VERSION
    	* VIRCADIA_USE_PREBUILT_QT
    	* HIFI_QT_BASE

--- hifi_qt.py
+++ hifi_qt.py
@@ -27,19 +27,83 @@ endif()
     def __init__(self, args):
         self.args = args
         self.configFilePath = os.path.join(args.build_root, 'qt.cmake')
-        self.version = os.getenv('VIRCADIA_USE_QT_VERSION', '5.12.3')
-
         self.assets_url = hifi_utils.readEnviromentVariableFromFile(args.build_root, 'EXTERNAL_BUILD_ASSETS')
 
-        defaultBasePath = os.path.expanduser('~/hifi/qt')
-        self.basePath = os.getenv('HIFI_QT_BASE', defaultBasePath)
-        if (not os.path.isdir(self.basePath)):
-            os.makedirs(self.basePath)
-        self.path = os.path.join(self.basePath, self.version)
-        self.fullPath = os.path.join(self.path, 'qt5-install')
-        self.cmakePath = os.path.join(self.fullPath, 'lib/cmake')
+        # OS dependent information
+        system = platform.system()
 
-        print("Using qt path {}".format(self.path))
+        qt_found = False
+
+        # Here we handle the 3 possible cases of dealing with Qt:
+        if os.getenv('VIRCADIA_USE_SYSTEM_QT'):
+            # 1. Using the system provided Qt. This is only recommended for Qt 5.15.0 and above,
+            # as it includes a required fix on Linux.
+            #
+            # This path only works on Linux as neither Windows nor OSX ship Qt.
+
+            if system != "Linux":
+                raise Exception("Using the system Qt is only supported on Linux")
+
+            cmake_paths = [ "lib64/cmake", "lib/cmake" ]
+            cmake_path_ok = False
+
+            # This makes the lockFile stuff happy. Needs to be writable.
+            self.path = tempfile.mkdtemp()
+
+            self.fullPath = '/usr'
+
+            # Find the cmake directory
+            for cp in cmake_paths:
+                self.cmakePath = os.path.join(self.fullPath, cp)
+                if os.path.isdir(self.cmakePath):
+                    cmake_path_ok = True
+                    break
+
+            if not cmake_path_ok:
+                raise Exception("Failed to find cmake directory. Looked under " + self.fullPath + " in " + (', '.join(cmake_paths)))
+
+            qt_found = True
+            print("Using system Qt")
+
+        elif os.getenv('VIRCADIA_QT_PATH'):
+            # 2. Using an user-provided directory.
+            # VIRCADIA_QT_PATH must point to a directory with a Qt install in it.
+
+            self.path = os.getenv('VIRCADIA_QT_PATH')
+            self.fullPath = self.path
+            self.cmakePath = os.path.join(self.fullPath, 'lib/cmake')
+
+            qt_found = True
+            print("Using Qt from " + self.fullPath)
+
+        else:
+            # 3. Using a pre-built Qt.
+            #
+            # This works somewhat differently from above, notice how path and fullPath are
+            # used differently in this case.
+            #
+            # In the case of an user-provided directory, we just use the user-supplied directory.
+            #
+            # For a pre-built qt, however, we have to unpack it. The archive is required to contain
+            # a qt5-install directory in it.
+
+            self.path = os.path.expanduser("~/vircadia-files/qt")
+            self.fullPath = os.path.join(self.path, 'qt5-install')
+            self.cmakePath = os.path.join(self.fullPath, 'lib/cmake')
+
+            if (not os.path.isdir(self.path)):
+                os.makedirs(self.path)
+
+            qt_found = os.path.isdir(self.fullPath)
+            print("Using a packaged Qt")
+
+        if qt_found:
+            # Sanity check, ensure we have a good cmake directory
+            if not os.path.isdir(os.path.join(self.cmakePath, "Qt5")):
+                raise Exception("Failed to find Qt5 directory under " + self.cmakePath)
+
+        # I'm not sure why this is needed. It's used by hifi_singleton.
+        # Perhaps it stops multiple build processes from interferring?
         lockDir, lockName = os.path.split(self.path)
         lockName += '.lock'
         if not os.path.isdir(lockDir):
@@ -47,12 +111,9 @@ endif()
 
         self.lockFile = os.path.join(lockDir, lockName)
 
-        if (os.getenv('VIRCADIA_USE_PREBUILT_QT')):
-            print("Using pre-built Qt5")
+        if qt_found:
+            print("Found pre-built Qt5")
             return
-
-        # OS dependent information
-        system = platform.system()
 
         if 'Windows' == system:
             self.qtUrl = self.assets_url + '/dependencies/vcpkg/qt5-install-5.12.3-windows3.tar.gz%3FversionId=5ADqP0M0j5ZfimUHrx4zJld6vYceHEsI'
@@ -60,7 +121,6 @@ endif()
             self.qtUrl = self.assets_url + '/dependencies/vcpkg/qt5-install-5.12.3-macos.tar.gz%3FversionId=bLAgnoJ8IMKpqv8NFDcAu8hsyQy3Rwwz'
         elif 'Linux' == system:
             import distro
-            dist = distro.linux_distribution()
 
             if distro.id() == 'ubuntu':
                 u_major = int( distro.major_version() )
@@ -73,21 +133,35 @@ endif()
                 elif u_major == 19 and u_minor == 10:
                     self.qtUrl = self.assets_url + '/dependencies/vcpkg/qt5-install-5.12.6-ubuntu-19.10.tar.xz'
                 elif u_major > 19:
+                    print("")
                     print("We don't support " + distro.name(pretty=True) + " yet. Perhaps consider helping us out?")
+                    self.showQtBuildInfo()
+                    print("")
                     raise Exception('LINUX DISTRO IS NOT SUPPORTED YET!!!')
                 else:
-                    print("Sorry, " + distro.name(pretty=True) + " is old and won't be officially supported. Please consider upgrading.");
+                    print("")
+                    print("Sorry, " + distro.name(pretty=True) + " is old and won't be officially supported. Please consider upgrading.")
+                    print("")
                     raise Exception('UNKNOWN LINUX DISTRO VERSION!!!')
             else:
+                print("")
                 print("Sorry, " + distro.name(pretty=True) + " is not supported. Please consider helping us out.")
-                print("It's also possible to build Qt for your distribution, please see the documentation at:")
-                print("https://github.com/vircadia/vircadia/tree/master/tools/qt-builder")
+                self.showQtBuildInfo()
+                print("")
                 raise Exception('UNKNOWN LINUX VERSION!!!')
         else:
             print("System      : " + platform.system())
             print("Architecture: " + platform.architecture())
             print("Machine     : " + platform.machine())
             raise Exception('UNKNOWN OPERATING SYSTEM!!!')
+
+    def showQtBuildInfo(self):
+        print("")
+        print("It's also possible to build Qt for your distribution, please see the documentation at:")
+        print("https://github.com/vircadia/vircadia/tree/master/tools/qt-builder")
+        print("")
+        print("Alternatively, you can try building against the system Qt by setting the VIRCADIA_USE_SYSTEM_QT environment variable.")
+        print("You'll need to install the development packages, and to have Qt 5.15.0 or newer. ")
 
     def writeConfig(self):
         print("Writing cmake config to {}".format(self.configFilePath))
